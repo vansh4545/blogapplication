c65f470de4b6e03c8e1a12b2bb6e21984c3ade43
vansh4545 <97082701+vansh4545@users.noreply.github.com>
2024-05-30 19:12:10 +0530

    second commit

--- controller/image_controller.js
+++ controller/image_controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = ''
+const url = 'http://localhost:8000'
 let gfs,gridfsBucket;
 const conn = mongoose.connection;
 conn.once('open',()=>{
