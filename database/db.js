1b7e741e7c93c4cf7113dfd1ec79d233b0f90c8f
vansh4545 <97082701+vansh4545@users.noreply.github.com>
2024-05-30 23:03:46 +0530

    fixed errors

--- database/db.js
+++ database/db.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const Connection= async () =>{
-  //const URL= process.env.MONGODB_URI;
+  const URL= process.env.MONGODB_URI;
   try{
-       await mongoose.connect(process.env.MONGODB_URI);
+       await mongoose.connect("mongodb+srv://vanshgupta4545:25July@2003@cluster0.ymwz4v3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
        console.log('database connected successfully');
     }
   catch(error){
+       console.log(URL);
        console.log('database connected unsuccesfully',error);
   }
 }
