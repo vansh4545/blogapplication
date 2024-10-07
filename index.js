b0a03a1450c40daf2d13ffdea65eabe4fbacba0b
vansh4545 <97082701+vansh4545@users.noreply.github.com>
2024-10-07 18:54:57 +0530

    fixed errors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app =express();
 app.use(bodyParser.json({extended:true}))
 
 app.use(cors());
-app.use(bodyParser.json({extended:true}))
+app.use(bodyParser.json({extended:true}))  
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',Router);
 
